4789098475bed1695ee0e4bd72ca5ac70155d9a4
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-05 20:49:05 +0200

    Endpoints

--- api-sequelize/src/index.js
+++ api-sequelize/src/index.js
@@ -1,6 +1,6 @@
 const app = require("./app/app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.DB_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running in http://localhost:${port}`)
--- api-sequelize/src/router/product.router.js
+++ api-sequelize/src/router/product.router.js
@@ -3,15 +3,31 @@ const router = require("express").Router();
 const { faker } = require("@faker-js/faker");
 const Products = require("../model/product.model");
 
-router.get("/products", (request, response) => {
-  response.send("I am Router");
+router.get("/products", async (request, response) => {
+  const products = await Products.findAll();
+    response.status(200).json({
+        ok: true,
+        status: 200,
+        body: products,
+    });
 });
 
-router.get("/products", (request, response) => {
-  response.send("I am Router");
+router.get("/products/:id", async (request, response) => {
+  const id = request.params.id;
+    const product = await Products.findOne({
+        where: {
+            id: id,
+        },
+    });
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: product,
+    });
 });
 
 router.post("/products", async (request, response) => {
+  const dataProducts = request.body;
   await Products.sync();
   const createProduct = await Products.create({
     productName: faker.commerce.product(),
@@ -25,12 +41,40 @@ router.post("/products", async (request, response) => {
   })
 });
 
-router.put("/products", (request, response) => {
-  response.send("I am Router");
+router.put("/products/:id", async (request, response) => {
+  const id = request.params.id;
+    const dataProducts = request.body;
+    const updateProduct = await Products.update(
+        {
+            product_name: dataProducts.product_name,
+            price: dataProducts.price,
+            inStock: dataProducts.inStock,
+        },
+        {
+            where: {
+                id: id,
+            },
+        }
+    );
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: updateProduct,
+    });
 });
 
-router.delete("/products", (request, response) => {
-  response.send("I am Router");
+router.delete("/products", async (request, response) => {
+  const id = request.params.id;
+    const deleteProduct = await Products.destroy({
+        where: {
+            id: id,
+        },
+    });
+    res.status(200).json({
+        ok: true,
+        status: 204,
+        body: deleteProduct,
+    });
 });
 
 module.exports = router;
