7f4765b39e67b190ae6e0988ce0d190b9bcaa9ef
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-05 16:20:35 +0200

    First configurations

---
+++ api-sequelize/src/app/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send(`Expressssss
+  Expresssssss`);
+});
+
+module.exports = app;
