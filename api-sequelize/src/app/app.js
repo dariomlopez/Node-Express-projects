d5149ac2f50e31d5b1bcec43948c0c6730bbbd8d
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-05 16:43:50 +0200

    Seeing if it's working

--- api-sequelize/src/app/app.js
+++ api-sequelize/src/app/app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send(`Expressssss
-  Expresssssss`);
+  response.send(`I'm doing something in express in real time`);
 });
 
 module.exports = app;
