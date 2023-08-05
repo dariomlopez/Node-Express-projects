706d69c588c881ca9c88e9f25faf988f6233830d
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-05 20:10:39 +0200

    Post a product

--- api-sequelize/src/model/product.model.js
+++ api-sequelize/src/model/product.model.js
@@ -1,5 +1,6 @@
-require("dotenv").config
-const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -7,7 +8,37 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: "mysql",
   }
+);
 
-)
+class Product extends Model {};
+
+Product.init({
+  productName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.FLOAT(10, 2),
+    allowNull: false
+  },
+  inStock: {
+    type: DataTypes.BOOLEAN
+  }
+}, {
+  sequelize,
+  modelName: "Product"
+});
+
+module.exports = Product;
+// const testConn = async () => {
+//   try {
+//     await sequelize.authenticate(); // Utilizar await aquí, ya que authenticate es una función asíncrona
+//     console.log("Connection has been established successfully.");
+//   } catch (err) {
+//     console.error("Unable to connect to the database:", err);
+//   }
+// };
+
+// testConn();
--- api-sequelize/src/router/product.router.js
+++ api-sequelize/src/router/product.router.js
@@ -1,6 +1,6 @@
-const { response } = require("../app/app");
-
 const router = require("express").Router();
+const Products = require("../model/product.model");
+const { faker } = require("@faker-js/faker");
 
 router.get("/products", (request, response) => {
   response.send("I am Router");
@@ -10,15 +10,21 @@ router.get("/products", (request, response) => {
   response.send("I am Router");
 });
 
-router.get("/products", (request, response) => {
+router.post("/products", async (request, response) => {
+  await Products.sync();
+  const createProduct = await Products.create({
+    productName: faker.commerce.product(),
+    price: faker.commerce.price(),
+    inStock: faker.datatype.boolean()
+  })
+  response.status(201)
+});
+
+router.put("/products", (request, response) => {
   response.send("I am Router");
 });
 
-router.get("/products", (request, response) => {
-  response.send("I am Router");
-});
-
-router.get("/products", (request, response) => {
+router.delete("/products", (request, response) => {
   response.send("I am Router");
 });
 
